3607b9e1afc2832c5e7d10af4b5fc3146d171051
Dan Tucker <furymuso@aim.com>
2017-09-21 14:34:04 +0100

    first commit with basic html form

---
+++ login.php
@@ -0,0 +1,28 @@
+<?php
+
+
+
+?>
+
+<!DOCTYPE html>
+<lang = en>
+<html>
+<body>
+<h1>Login Page</h1>
+    <div>
+        <div>
+            <h2>Username:</h2>
+            <input type="text" name="Username">
+        </div>
+        <div>
+            <h2>Password:</h2>
+            <input type="text" name="Password">
+        </div>
+        <div>
+            <p><br></p>
+            <input type="submit" value="Submit">
+        </div>
+    </div>
+</body>
+</html>
+
